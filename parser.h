becb2856907686c3726f171473d5286e700b94ab
Pedro Lucas <pedroplpa@hotmail.com>
2017-10-24 20:53:24 -0300

    parser.h added

---
+++ parser.h
@@ -0,0 +1,17 @@
+/**
+ *  Simple Scripting Language
+ *
+ * @author: Naum Azeredo Fernandes Barreira, Pedro Lucas Porto Almeida
+ *
+ */
+
+#ifndef SSL_PARSER
+#define SSL_PARSER
+
+#include "parser_gen.h"
+#include "queue.h"
+#include "semantics.h"
+
+void parse();
+
+#endif //SSL_PARSER
